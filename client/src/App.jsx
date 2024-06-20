98f6be702369adc966accb05e7e93a2db6f2dcb3
Abhishek Kumar <abhishek2021005@gmail.com>
2024-06-20 18:01:08 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,10 @@ import Routes from "./Routes";
 
 function App() {
  
-  axios.defaults.baseURL = 'http://localhost:4040';
+  // axios.defaults.baseURL = 'http://localhost:4040';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
+ 
   // axios.defaults.baseURL = 'http://localhost:5173';
   axios.defaults.withCredentials = true;
   return (
